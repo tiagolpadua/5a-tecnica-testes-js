7c7af40b686975531d3c9fe64015a803c7032a27
Raphael Lacerda <lacerdaph@gmail.com>
2018-02-26 14:56:01 -0300

    testes com mocks

---
+++ src/todosEnderecos.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var todosEnderecos = (function ($) {
+
+    var buscarPorCep = function (cep) {
+
+        var p1 = new Promise(
+
+            function (resolve, reject) {
+
+                var endereco = {
+                    cep: '70.673-410',
+                    logradouro: 'SQSW 304 bloco j'
+                };
+
+                resolve(endereco);
+
+            }
+        );
+        return p1;
+    };
+
+    return {
+        buscarPorCep: buscarPorCep,
+    };
+
+})(jQuery);
